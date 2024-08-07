573c158d91f8e5bee87b93d343bb072be22a3dc8
keungkyang <zhfldkssm@gmail.com>
2024-08-07 14:49:54 +0900

    add new

---
+++ Source/L20240807_BP2/L20240807_BP2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L20240807_BP2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20240807_BP2, "L20240807_BP2" );
